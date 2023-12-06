3c84d7265c62033f4b9e1cb44f04ec4c487f5a99
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-12-06 21:15:22 +0300

    fix: use typescript prompt sync

--- src/data.ts
+++ src/data.ts
@@ -1,24 +1,22 @@
 import { User, Wallet } from "./model";
 
 export class Data {
-    constructor(){
+  constructor() {}
 
-    }
+  private walletData: Wallet[] = [];
+  private userData: User[] = [];
+  user1: User = new User("user1", "password1");
+  root: User = new User("root", "root");
+  wallet1: Wallet = new Wallet(this.user1, 20000);
+  walletRoot: Wallet = new Wallet(this.root, 99999);
 
-    private walletData: Wallet[];
-    private userData: User[];
-    user1: User = new User("user1", "password1");
-    root: User = new User("root", "root");
-    wallet1: Wallet = new Wallet(this.user1, 20000);
-    walletRoot: Wallet = new Wallet(this.root, 99999);
+  public findUserData(): User[] {
+    this.userData.push(this.user1, this.root);
+    return this.userData;
+  }
 
-    public findUserData(): User[] {
-        this.userData.push(this.user1, this.root);
-        return this.userData;
-    }
-
-    public findWalletData(): Wallet[] {
-        this.walletData.push(this.wallet1, this.walletRoot);
-        return this.walletData;
-    }
+  public findWalletData(): Wallet[] {
+    this.walletData.push(this.wallet1, this.walletRoot);
+    return this.walletData;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import { Service } from "./service";
-const readline = require('prompt-sync')();
+import prompt from "prompt-sync";
+const readline = prompt();
 
 const service: Service = new Service();
 
-while(true) {
-    console.log("Welcome to wallet ts ----");
-    console.log("Please login");
-    const username = readline("- enter username: ");
-    const password = readline("- enter password: ");
-    if(service.isAuthenticated(username, password)) {
-
-    }
-    else {
-        break     
-    }  
+while (true) {
+  console.log("Welcome to wallet ts ----");
+  console.log("Please login");
+  const username = readline("- enter username: ");
+  const password = readline("- enter password: ");
+  if (service.isAuthenticated(username, password)) {
+  } else {
+    break;
+  }
 }
